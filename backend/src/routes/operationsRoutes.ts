0a657da2beb2647bdb31a6bb8939bcfa64c31bb2
DmitriiBank <dm.skakov@gmail.com>
2026-04-18 14:54:08 +0300

    add auth protect to health API

--- backend/src/routes/operationsRoutes.ts
+++ backend/src/routes/operationsRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { Role } from "@prisma/client";
 
 import {createOperation, getOperationStatus} from "../controllers/operationsController";
+import { authorize, protect } from "../middleware/authMiddleware";
 
 
 export const operationsRouter = express.Router();
 
+operationsRouter.use(protect);
+operationsRouter.use(authorize(Role.OPERATOR, Role.ADMIN));
 operationsRouter.post('/health', createOperation);
-operationsRouter.get('/:id', getOperationStatus)
+operationsRouter.get('/:id', getOperationStatus);
