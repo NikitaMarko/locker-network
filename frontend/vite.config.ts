a3fad651e5d842a27432d312223521bb3c4a7278
NMarkovskii <nikitamaxi@inbox.ru>
2026-04-26 23:55:12 +0300

    fixed bugs for build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/',
     server: {
         headers: {
             "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
             "Cross-Origin-Embedder-Policy": "unsafe-none",
         },
     },
+
 })
